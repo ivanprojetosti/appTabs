5da2842928928f73f0584a53b9129bb37811ff3d
Ivan Sena <ivanprojetosti@gmail.com>
2021-02-25 22:53:16 -0300

    adicionado novas pages

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -7,28 +7,48 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children: [
+      
+
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'pain',
+        loadChildren: () => import('../pain/pain.module').then( m => m.PainPageModule)
       },
+      // {
+      //   path: 'sintomas',
+      //   loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+      // },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'symptoms',
+        loadChildren: () => import('../symptoms/symptoms.module').then( m => m.SymptomsPageModule)
       },
+
+
+      // {
+      //   path: 'medicamentos',
+      //   loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      // },
+
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'medicament',
+        loadChildren: () => import('../medicament/medicament.module').then( m => m.MedicamentPageModule)
       },
+
+      {
+        path: 'options',
+        loadChildren: () => import('../options/options.module').then( m => m.OptionsPageModule)
+      },
+
+
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/pain',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/pain',
     pathMatch: 'full'
   }
 ];
